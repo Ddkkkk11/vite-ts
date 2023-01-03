685431b9390dd52d793acd0e0acab56508bae0f1
xdd <45672942@qq.com>
2023-01-03 22:42:24 +0800

    any unknow类型

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,9 @@
-// const button = document.getElementById("#root");
-// if (button) {
-  // button// 如果button存在则button为HTMLElement
-  // button.addEventListener("click", () => {}, undefined);
-// }
-type A = null;
-const a: A = null;//声明一个集合为空
-const s: symbol = Symbol('s');
-console.log('s', s);
-//this
-type Person = {
-  name: string
-}
-type F = (this: Person, name: string) => void;
-const Fn: F = function(name) {
-  console.log(this.name);
-}
-Fn.call({name: "qwe"}, "Roll"); //如果使用this必须放到第一个 而且只能叫this
-const x = {};
+//any unknow ？？？
+const a: unknown = 1;
+a.fixed();//声明了一个变量 类型是unknow 值是1但是无法调用.fixed() 因为类型是unknow 不知道
+//unknow 适合 从外部获取的 但是这个值我无法知道
+const b:unknown = ajax.get('url');
+//从远处获取到的值 先把他盖住 然后get得到的值被改在下面 需要使用的时候断言
+const c = (b as number);
+// never一个不包含任何元素的集合叫空集
 export {}
