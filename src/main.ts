bd3984c8345ad9828520e8019be71845b77813a5
xdd <45672942@qq.com>
2023-01-03 22:54:35 +0800

    never 推断

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,18 @@ const b:unknown = ajax.get('url');
 //从远处获取到的值 先把他盖住 然后get得到的值被改在下面 需要使用的时候断言
 const c = (b as number);
 // never一个不包含任何元素的集合叫空集
+type A = number & string; //A的类型是number且是string
+const newA: A = '' //new A的类型就是never
+//never是做类型推断的
+type nev = string | number | boolean;
+const obj: nev = 'asd' as any;
+if (typeof obj === 'string') {
+  obj.toLocaleUpperCase()
+}else if(typeof obj === 'number') {
+  obj.toFixed();
+}else if (typeof obj === 'boolean') {
+  obj.valueOf();
+}else {
+  obj
+}
 export {}
