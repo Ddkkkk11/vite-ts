12cf1ad07f9aa39e4078d67375f8243c2f190cd5
xdd <45672942@qq.com>
2023-01-15 18:03:30 +0800

    基础类型总结

---
+++ README.md
@@ -0,0 +1,2 @@
+## 学习进度
+[进度](https://www.bilibili.com/video/BV1pT411A7Y5/?spm_id_from=pageDriver&vd_source=26b5f24919aeb671ba1dbff34904c160)
---
+++ src/基本类型总结.ts
@@ -0,0 +1,16 @@
+type A = boolean;
+const a: A = false;
+console.log('a', a);
+type B = Object;//包含所有的可以封装的类型  可以把string undefined 给Object 类似于any
+type C = object;
+const c: C = 1; //err 不能把number赋值给object  //类型于any
+type Fn = () => void;
+const fn: Fn = () => {
+  return 123;
+}
+type Test = string & number;
+const test: Test = '' //当一个类型即是string也是number的时候这个类型就是never  走到这步的时候检查下
+const newNever: never = ''//
+type As = number | string;
+const as: As = '1'
+export {}
