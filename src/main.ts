bc790b13471f3fb6d054efea11cc609cd8c60f3b
xdd <45672942@qq.com>
2023-01-20 16:14:31 +0800

    unknow类型补充

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,28 @@
-// 辨别类型总结
-type Action = { type: 'getUser', id: string }
-  | {
-    type: 'createUser', attributes: {
-      id: string,
-      name: string
-    }
+// type any = noErrorType;
+// type unknow = 所有类型联合
+
+//any 不是所有类型的联合
+//比如
+const func = (a: string | number ) => {
+  // a.toLocaleString 只能用交集含有的方法
+
+}
+//但是如果使用any
+const anyFunc = (a: any) => {
+  a.toFixed();
+  a.toString(); //可以用所有类型的方法
+  // const b: never = a;
+}
+//ts绝大部分规则对any不生效
+
+//什么类型是所有类型的联合 //unknow
+const f = (a: unknown) => {
+  if (typeof a === 'string') {
+    
+  }else if( typeof a === 'number') {
+    a
+    
   }
-  | { type: 'deleteUser', id: string }
-  | {
-    type: 'updateUser', attributes: {
-      id: string,
-      name: string
-    }
-  }
-  const actionFunc = (action: Action) => {
-    if (action.type === 'getUser') {
-      action
-    }
-  }
-//相同属性可以进行类型收窄 但是kind必须是基本数据类型 不能变成对象
-export { }
+}
+
+export {}
